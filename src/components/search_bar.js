2e9a41913ec9b4bac3de4551700c0187eeebf48b
Michele Nasti <michele.nasti@gmail.com>
2018-01-16 11:02:16 +0100

    youtube search

---
+++ src/components/search_bar.js
@@ -0,0 +1,45 @@
+import React, {
+  Component
+} from 'react';
+
+// //functional component
+// const SearchBar = () => {
+//   return <input />;
+// }
+
+//class based
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //solo nel costruttore si setta lo stato in questo modo 
+    this.state = {
+      term: ''
+    };
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return ( 
+      <div>
+
+        <input 
+
+        value={this.state.term}
+          //when the state changes, the component gets rerendered
+          //always manipulate the state using this.setState({...})
+        onChange = {
+          event => this.setState({
+            term: event.target.value
+          })
+        } />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBlk2NfU0x2cNcx2JhKbBjn2h_3g4PH9T0';
 
 // Create a new component. This component should produce some html 
-const App = function() {
-  return <div>Hi!</div>;
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state({ videos: []})
+
+    YTSearch({key: API_KEY, term: 'michele nasti'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() { 
+  <div>
+      <SearchBar />
+  </div>
+  }
 }
 
 console.log(App);
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-React.render(App);
+ReactDOM.render( < App / > , document.querySelector('.container'));
