522a3636abf1eaf9176f85a210265e5dbee32c85
Michele Nasti <michele@bemind.me>
2018-01-16 12:20:38 +0100

    added video list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBlk2NfU0x2cNcx2JhKbBjn2h_3g4PH9T0';
 
@@ -10,21 +11,23 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state({ videos: []})
+    this.state = { videos: [] }
 
     YTSearch({key: API_KEY, term: 'michele nasti'}, (videos) => {
       this.setState({videos});
     });
   }
 
+
   render() { 
-  <div>
-      <SearchBar />
-  </div>
+  return (
+    <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+    </div>
+    );
   }
 }
-
-console.log(App);
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render( < App / > , document.querySelector('.container'));
